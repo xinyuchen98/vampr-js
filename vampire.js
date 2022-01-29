a66879338798689f2fe6aaefeb1a89a2eb4d05a5
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-29 03:35:47 -0700

    Intro to Trees

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let vampire = this;
+    let number = 0;
+    while (vampire.creator) {
+      vampire = vampire.creator;
+      number++;
+    }
+    return number;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const numberThis = this.numberOfVampiresFromOriginal;
+    const numberThat = vampire.numberOfVampiresFromOriginal;
+    if (numberThis < numberThat) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -41,4 +53,3 @@ class Vampire {
 }
 
 module.exports = Vampire;
-
